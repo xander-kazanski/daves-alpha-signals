70040f109a3060059add7e575f08e83e12562f32
Xander Kazanski <xander.kazanski@gmail.com>
2025-04-26 09:10:41 -0400

    Add wrangler as a dev dependency in package.json

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ async function fetchStockData() {
         const stockData = await Promise.all(
             tickersArr.map(async (ticker) => {
                 apiMessage.innerText = `Fetching data for ${ticker}...`
-                const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/${dates.startDate}/${dates.endDate}?apiKey=${import.meta.env.VITE_POLYGON_API_KEY}`;
+                const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/${dates.startDate}/${dates.endDate}?apiKey=${env.POLYGON_API_KEY}`;
                 const response = await fetch(url)
 
                 if (response.ok) {
